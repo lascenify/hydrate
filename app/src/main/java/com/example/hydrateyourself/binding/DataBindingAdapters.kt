35436f1d706c152dc33b543fa6eeae1ee9331059
lascenify <ascensalmanez@gmail.com>
2020-04-02 10:30:19 +0200

    Deleted binding directory

--- app/src/main/java/com/example/hydrateyourself/binding/DataBindingAdapters.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hydrateyourself.binding
-
-import android.widget.TextView
-import androidx.databinding.BindingAdapter
-import com.example.hydrateyourself.util.PreferenceUtilities
-
